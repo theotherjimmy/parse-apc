e7069ac4272030cba66e7693509c2c0829c8ba26
Jimmy Brisson <theotherjimmy@gmail.com>
2016-04-17 10:38:58 -0500

    cleaned up a pile of warnings

--- parse-apc.hs
+++ parse-apc.hs
@@ -18,6 +18,7 @@ import Data.List (intercalate)
 import Data.Maybe
 import Data.Word
 import Options.Applicative
+import qualified Options.Applicative as Opt
 import Prelude hiding (take)
 import Text.XML.HXT.Core
 import Text.XML.HXT.XPath.Arrows
@@ -26,18 +27,22 @@ data Arguments = Arguments { inputfile :: String
                            , capture :: String
                            }
 
+argParser :: Opt.Parser Arguments
 argParser = Arguments <$> argument str (metavar "00000000")
   <*> argument str (metavar "capture.xml")
 
+parseAPC :: BSL.ByteString -> Maybe [Either String Frame]
 parseAPC = maybeResult . parse fileParser
 
+fileParser :: A.Parser [Either String Frame]
 fileParser = manyTill apcFrame endOfInput
 
+apcFrame :: A.Parser (Either String Frame)
 apcFrame =  parseFrame <$> (anyWord32le >>= take . fromIntegral)
 
+parseFrame :: BSS.ByteString -> Either String Frame
 parseFrame =  parseOnly frameParser
-
-frameParser = packed32 <* packed_ >>= frameFromNum
+  where frameParser = packed32 <* packed_ >>= frameFromNum
 
 data Frame = Counter { counterTimestamp :: Word64
                      , counterKey :: Word32
@@ -60,16 +65,16 @@ frameFromNum 5 = BlockCounter . blockToMaps
   <$> manyTill ((,) <$> packed32 <*> packed64) endOfInput
 frameFromNum _ = pure DontCare
 
+blockToMaps :: (Num k, Ord k, Foldable t) => t (k, a) -> [(a, M.Map k a)]
 blockToMaps m = foldl doMap [] m
-  where doMap [] (0, value) = [(value, M.empty)]
-        doMap x (0, value) = (value,M.empty) : x
-        doMap ((ts,x):xs) (key, value) = (ts,M.insert key value x) : xs
+  where doMap [] (0, v) = [(v, M.empty)]
+        doMap x (0, v) = (v,M.empty) : x
+        doMap ((ts,x):xs) (key, v) = (ts,M.insert key v x) : xs
         doMap x _ = x
 
-stringN str = word8 (fromIntegral $ BSS.length str) *> string str
+stringN :: BSS.ByteString -> A.Parser BSS.ByteString
+stringN s = word8 (fromIntegral $ BSS.length s) *> string s
 
-takeString :: A.Parser BSS.ByteString
-takeString = BSS.copy <$> (packed32 >>= take . fromIntegral)
 takeString_ :: A.Parser ()
 takeString_ =  (packed32 >>= void . take . fromIntegral)
 
@@ -92,22 +97,26 @@ packed_ = void $ scan False highBitSet
                               | otherwise = Just True
         highBitSet True _ = Nothing
 
+prettyPrint :: Foldable t => M.Map Word32 b -> t Frame -> IO ()
 prettyPrint keys l = foldlM helper (0, M.map (const 0) keys) l >>= \(ts, out) -> printRow ts out
   where helper (ts, prev) a@(Counter nts key val) | nts /= ts = printRow ts prev >> helper (nts, prev) a
                                                   | otherwise = pure (nts, M.insert key val prev)
-        helper b a@(BlockCounter x) = foldlM blockhelper b x
-          where blockhelper (ts, prev) (nts, map) | (nts /= ts) = printRow ts prev >> blockhelper (nts, prev) (nts, map)
-                                                  | otherwise = pure (nts, M.union map prev)
+        helper b (BlockCounter x) = foldlM blockhelper b x
+          where blockhelper (ts, prev) (nts, m) | (nts /= ts) = printRow ts prev >> blockhelper (nts, prev) (nts, m)
+                                                | otherwise = pure (nts, M.union m prev)
         helper b _= pure b
 
+printRow :: (Eq a, Num a, Show a, Show b) => a -> M.Map k b -> IO ()
 printRow 0 _ = pure ()
 printRow ts m = putStrLn $ mappend (show ts <> ", ") $ intercalate ", " $ map show $ M.elems m
 
+getKeys :: IOSLA (XIOState ()) XmlTree (Word32, String)
 getKeys = isElem >>> hasName "counter" >>> proc c -> do
   key <- getAttrValue "key" -< c
   name <- getAttrValue "type" -< c
   returnA -< (read key :: Word32, name)
 
+main :: IO ()
 main = execParser opts >>= \args -> do
   keys <- M.fromList <$> (runX $ readDocument [] (capture args)
                           >>> getXPathTrees "/captured/counters/counter"
